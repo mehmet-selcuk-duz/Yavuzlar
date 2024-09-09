449ac86a9ca50737392b058047aabfdec6589d6e
Mehmet Selçuk Düz <51099980+mehmet-selcuk-duz@users.noreply.github.com>
2024-09-09 17:45:55 +0300

    Add files via upload

--- Ǫuest Application Php/sorular.php
+++ Ǫuest Application Php/sorular.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
+    header('Location: login.php');
+    exit();
+}
+
 $db = new PDO('sqlite:veriler.db');
 
 $query = "SELECT * FROM sorular";
